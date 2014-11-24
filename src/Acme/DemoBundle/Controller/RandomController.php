323f03b0fc6d99801af7d43c8e05ccea64867ffc
Killsoft <killsoft1692@yandex.ua>
2014-11-25 01:36:43 +0200

    first commit

---
+++ src/Acme/DemoBundle/Controller/RandomController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RandomController extends Controller
+{
+    public function indexAction($limit)
+    {
+        /**
+         * @Route("/random/{limit}")
+        */
+        $number = rand(1, $limit);
+        return $this-> render(
+            'AcmeDemoBundle:Random:index.html.twig',
+            array('number' => $number)
+        );
+        // render a PHP template instead
+        // return $this->render(
+        //        'AcmeDemoBundle:Random:index.html.php',
+        // array('number' => $number)
+        // );
+    }
+}
